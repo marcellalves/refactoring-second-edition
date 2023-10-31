cae0c161ed664f1ad37bac9da11c4e43cea9233d
Marcell Alves <marcell.alves.t_ciet@grupoboticario.com.br>
2023-10-31 14:21:46 -0300

    refactor: add expect assertion style with chai

--- Chapter4/tests/Province.test.js
+++ Chapter4/tests/Province.test.js
@@ -1,14 +1,15 @@
 import Province from "../Province.js";
 import sampleProvinceData from "../SampleProvinceData.js";
 import assert from "assert";
+import { expect } from "chai";
 
 describe("province", function () {
   it("shortfall", function () {
     const asia = new Province(sampleProvinceData());
-    assert.equal(asia.shortfall, 5);
+    expect(asia.shortfall).equal(5);
   });
   it("profit", function () {
     const asia = new Province(sampleProvinceData());
-    assert.equal(asia.profit, 230);
+    expect(asia.profit).equal(230);
   });
 });
